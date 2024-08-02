4857a18305ef029aea8f17d0b00e4edd6a856c48
Sami <47335566+ssaammii5@users.noreply.github.com>
2024-08-03 03:47:33 +0600

    commit

--- app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
+++ app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
@@ -9,15 +9,23 @@ import kotlinx.coroutines.launch
 class MainViewModel : ViewModel() {
 
     private val _categoryState = mutableStateOf(RecipeState())
-    val categoryState : State<RecipeState> = _categoryState
+    val categoryState: State<RecipeState> = _categoryState
 
-    private fun fetchCategories(){
+    init{
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
         viewModelScope.launch {
             try {
                 val response = recipeService.getCategories()
-                _categoryState.value
-            } catch (e : Exception){
-                _categoryState.value=_categoryState.value.copy(
+                _categoryState.value = _categoryState.value.copy(
+                    list = response.categories,
+                    loading = false,
+                    error = null
+                )
+            } catch (e: Exception) {
+                _categoryState.value = _categoryState.value.copy(
                     loading = false,
                     error = "Error fetching categories ${e.message}"
                 )
